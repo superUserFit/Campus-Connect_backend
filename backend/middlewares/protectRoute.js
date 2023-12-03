b5e736358222170bcb06308b59ed4638ee98e596
NightOwl <102517523+superUserFit@users.noreply.github.com>
2023-12-03 21:21:41 +0800

    Update protectRoute.js

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -5,6 +5,7 @@ const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
 
+		console.log("Token: ", token);
 		if (!token) return res.status(401).json({ message: "Unauthorized" });
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
