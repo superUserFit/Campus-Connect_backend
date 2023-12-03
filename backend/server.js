d64b1d7c280f85c4d38b8a47388e0ad182d94369
NightOwl <102517523+superUserFit@users.noreply.github.com>
2023-12-03 22:30:56 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,10 @@ cloudinary.config({
 app.use(express.json({ limit: "900mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+	origin: "https://campus-connect-frontend-six.vercel.app/",
+	credentials: true
+));
 
 // Routes
 app.use("/api/users", userRoutes);
